be922206358d6a7d94da9b5ff6b2d325fb7f0db6
seanhelvey <sean.helvey@gmail.com>
2019-03-18 14:27:15 -0600

    finished code improvements

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,30 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
-    describe '#index' do
-        it 'should return success response' do
-            get :index
-            expect(response).to have_http_status(:ok)
-        end
-    end
 
     describe '#index' do
+        subject { get :index }
+
+        it 'should return success response' do
+            subject
+            expect(response).to have_http_status(:ok)
+        end
+
         it 'should return properly formatted json' do
-            create_list :article, 2
-            get :index
-            json = JSON.parse(response.body)
-            json_data = json['data']
-            expect(json_data.length).to eq(2)
-            expect(json_data[0]['attributes']).to eq({
-                "title" => "My awesome title 1",
-                "content" => "My awesome content 1",
-                "slug" => "my-awesome-slug-1"
-            })
-            expect(json_data[1]['attributes']).to eq({
-                "title" => "My awesome title 2",
-                "content" => "My awesome content 2",
-                "slug" => "my-awesome-slug-2"
-            })
+            articles = create_list :article, 2
+            subject
+            articles.each_with_index do |article, index|
+                expect(json_data[index]['attributes']).to eq({
+                    "title" => article.title,
+                    "content" => article.content,
+                    "slug" => article.slug
+                })
+            end
         end
     end
 
